5a582de30b55680d718a3a67536a742048e11d4b
Camilla <logikasciuro@gmail.com>
2023-08-23 13:51:44 +0000

    🎉  Begin Project

---
+++ src/test/java/com/ecureuill/ada/avanade/orderapi/OrderapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecureuill.ada.avanade.orderapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
