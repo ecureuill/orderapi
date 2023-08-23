15b906f975a4a7df18cbfd18606876bc79c5e197
Camilla <logikasciuro@gmail.com>
2023-08-23 13:54:03 +0000

    🛂 (CORS) Enable request from any method to any endpoint

---
+++ src/main/java/com/ecureuill/ada/orderapi/infra/security/CORSConfig.java
@@ -0,0 +1,13 @@
+package com.ecureuill.ada.orderapi.infra.security;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class CORSConfig implements WebMvcConfigurer{
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("*")
+                .allowedOrigins("*");
+    }
+}
