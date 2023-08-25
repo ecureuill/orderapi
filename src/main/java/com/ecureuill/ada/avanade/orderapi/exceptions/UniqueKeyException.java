49ea6f40f130e35e47d6ab54ebf880a7b0c1a5d3
Camilla <logikasciuro@gmail.com>
2023-08-25 22:23:43 +0000

    🥅 (UniqueKeyException) create exception

---
+++ src/main/java/com/ecureuill/ada/avanade/orderapi/exceptions/UniqueKeyException.java
@@ -0,0 +1,7 @@
+package com.ecureuill.ada.avanade.orderapi.exceptions;
+
+public class UniqueKeyException extends Exception{
+    public UniqueKeyException(String message) {
+        super(message);
+    }
+}
